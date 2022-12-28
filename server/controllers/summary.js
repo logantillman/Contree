50d9be218473a764e4544c4c2226ed140fa439cb
logant08 <logantillman@ymail.com>
2022-12-27 22:48:39 -0600

    Implement basic CRUD methods

--- server/controllers/summary.js
+++ server/controllers/summary.js
@@ -1,14 +1,34 @@
+import Summary from '../models/Summary.js';
+
 export const getSummary = async(req, res) => {
     try {
-        res.status(200).json({ message: "we did it" });
+        const summaries = await Summary.find();
+
+        res.status(200).json(summaries);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 };
 
 export const getSummaryById = async(req, res) => {
+    const id = req.params.id;
+
     try {
-        res.status(200).json(req.params);
+        const summary = await Summary.findById(id);
+
+        res.status(200).json(summary);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+export const createSummary = async(req, res) => {
+    const categories = req.body;
+
+    const summary = new Summary(categories);
+    try {
+        await summary.save();
+        res.status(200).json({ message: "success" });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
