53769602e96c44c86dfa291b6607520b296cc7d8
logant08 <logantillman@ymail.com>
2023-04-15 23:04:12 -0500

    Require unique emails

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose';
 const userSchema = mongoose.Schema({
     email: {
         type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
         required: true
     },
     token: {
