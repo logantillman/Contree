35e127aadb7391718d54422e382cbe3c68d84d74
logant08 <logantillman@ymail.com>
2023-04-16 22:43:05 -0500

    Add new auth routes

--- server/index.js
+++ server/index.js
@@ -2,15 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import cookies from 'cookie-parser'
 
-import { registerUser, loginUser, verifyJWT } from './controllers/auth.js';
+import { registerUser, loginUser, logoutUser, refresh } from './controllers/auth.js';
+import { verifyJWT } from './middleware/verifyJWT.js';
 import summaryRoutes from './routes/summaryRoutes.js';
 import plaidRoutes from './routes/plaidRoutes.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 dotenv.config({ path: './config.env' });
 const app = express();
 
+app.use(cookies());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -20,6 +24,8 @@ const PORT = process.env.PORT || 3001;
 
 app.post('/register', registerUser);
 app.post('/login', loginUser);
+app.post('/logout', logoutUser);
+app.get('/refresh', refresh);
 
 app.use('/summary', verifyJWT, summaryRoutes);
 app.use('/plaid', verifyJWT, plaidRoutes);
