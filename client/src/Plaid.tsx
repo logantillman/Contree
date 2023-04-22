a7ebc389abe01c783b01462b2ff958df86b7a044
logant08 <logantillman@ymail.com>
2023-04-22 14:15:18 -0500

    Rename plaid component

--- client/src/Plaid.tsx
+++ client/src/Plaid.tsx
@@ -3,12 +3,16 @@
 // obtain a link token to be used in the Link component
 import React, { useEffect, useState } from 'react';
 import { usePlaidLink } from 'react-plaid-link';
-const App = () => {
+const Plaid = () => {
+  console.log("HERERHEEHRERE");
   const [linkToken, setLinkToken] = useState(null);
   const generateToken = async () => {
     console.log("generating token");
-    const response = await fetch('http://localhost:3000/plaid/create-link-token/63d4bdd433752bb59e583a32', {
+    const response = await fetch('http://localhost:3000/plaid/create-link-token', {
       method: 'POST',
+      headers: {
+        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MDJlMzhjODUyYzcxODZjZDJmZGZmMCIsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJpYXQiOjE2ODE2MjE1MzQsImV4cCI6MTY4MTcwNzkzNH0.p2tEIWRcnDckqwCRV54vLa9eBiSjrMMooN5uH6B-lQo"
+      }
     });
     const data = await response.json();
     console.log(data);
@@ -30,14 +34,16 @@ const Link: React.FC<LinkProps> = (props: LinkProps) => {
   const onSuccess = React.useCallback((public_token, metadata) => {
     // send public_token to server
     console.log("sending public token to server");
-    const response = fetch('http://localhost:3000/plaid/exchange-public-token/63d4bdd433752bb59e583a32', {
+    const response = fetch('http://localhost:3000/plaid/exchange-public-token', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MDJlMzhjODUyYzcxODZjZDJmZGZmMCIsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJpYXQiOjE2ODE2MjE1MzQsImV4cCI6MTY4MTcwNzkzNH0.p2tEIWRcnDckqwCRV54vLa9eBiSjrMMooN5uH6B-lQo"
       },
       body: JSON.stringify({ public_token }),
     });
     // Handle response ...
+    console.log(response);
   }, []);
   const config: Parameters<typeof usePlaidLink>[0] = {
     token: props.linkToken!,
@@ -50,4 +56,4 @@ const Link: React.FC<LinkProps> = (props: LinkProps) => {
     </button>
   );
 };
-export default App;
+export default Plaid;
