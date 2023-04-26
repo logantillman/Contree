a757c34e775a141e395e892b24212bba6f382009
logant08 <logantillman@ymail.com>
2023-04-25 23:01:00 -0500

    Implement logout functionality

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,6 +1,26 @@
-const Logout = () => {
+import { Button } from "@mui/material";
+import axios from 'axios';
+import { useContext } from "react";
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "../context/AuthContext";
+
+const logoutUser = async() => {
+    return await axios.post('http://localhost:3000/auth/logout', { withCredentials: true });
+}
+
+const Logout = (props) => {
+    const navigate = useNavigate();
+    const [ , setAuthenticated] = useContext(AuthContext);
+
+    const handleLogOut = async() => {
+        await logoutUser();
+        props.setToken(null);
+        setAuthenticated(false);
+        navigate("/login");
+    }
+ 
     return (
-        <h1>logout</h1>
+        <Button variant="contained" onClick={handleLogOut}>Logout</Button>
     )
 };
 
